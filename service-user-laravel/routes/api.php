29ae8871219fe61d248552bbc9338c5b38ec8f16
lidxgoesgiri <gusgirik@gmail.com>
2026-04-29 14:48:18 +0700

    Menambahkan fitur register dan login

--- service-user-laravel/routes/api.php
+++ service-user-laravel/routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
